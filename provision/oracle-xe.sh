f228f992324f32db4e8e1ac5c1ec868f2bc10353
pau.ferrer-ocana <crazyserver@gmail.com>
2015-01-14 16:00:51 +0100

    Add Oracle instant client support

--- provision/oracle-xe.sh
+++ provision/oracle-xe.sh
@@ -16,6 +16,8 @@ sudo alien --scripts oracle-xe-11.2.0-1.0.x86_64.rpm
 sudo dpkg -i oracle-xe_11.2.0-2_amd64.deb
 popd
 
+rm -Rf /tmp/Disk1
+
 sudo update-rc.d oracle-xe defaults 80 01
 
 sudo rm /dev/shm
@@ -28,4 +30,45 @@ sudo cp /vagrant/provision/oracle/oracle-xe /etc/init.d/oracle-xe
 
 sudo service oracle-xe configure responseFile=/vagrant/provision/oracle/oracle.conf >> /tmp/xe-install.log
 
-#sudo pecl install oci8
+
+if [ ! -f /vagrant/provision/oracle/oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.rpm ]; then
+    echo 'Download oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64 and save it on provision folder'
+    exit -1
+fi
+
+sudo cp /vagrant/provision/oracle/oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.rpm /tmp
+
+pushd /tmp
+sudo alien --scripts oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.rpm
+sudo dpkg -i oracle-instantclient12.1-basic_12.1.0.2.0-2_amd64.deb
+sudo rm -f oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.rpm oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.rpm
+popd
+
+export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib/:$LD_LIBRARY_PATH
+export ORACLE_HOME=/usr/lib/oracle/12.1/client64
+
+sudo ldconfig
+
+if [ ! -f /vagrant/provision/oracle/oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm ]; then
+    echo 'Download oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64 and save it on provision folder'
+    exit -1
+fi
+
+sudo cp /vagrant/provision/oracle/oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm /tmp
+
+pushd /tmp
+sudo alien --scripts oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm
+sudo dpkg -i oracle-instantclient12.1-devel_12.1.0.2.0-2_amd64.deb
+sudo rm -f oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm oracle-instantclient12.1-devel_12.1.0.2.0-2_amd64.deb
+popd
+
+sudo ln -s /usr/include/oracle/12.1/client64 $ORACLE_HOME/include
+
+echo instantclient,/usr/lib/oracle/12.1/client64/lib | sudo pecl install oci8
+
+echo "extension=oci8.so" | sudo tee -a /etc/php5/mods-available/oci8.ini
+
+sudo ln -s /etc/php5/mods-available/oci8.ini /etc/php5/apache2/conf.d/30-oci8.ini
+sudo ln -s /etc/php5/mods-available/oci8.ini /etc/php5/cli/conf.d/30-oci8.ini
+
+sudo service apache2 restart
