47fe19c73ac0b51bc59a3d8b7d9fa93feed6aabb
pau.ferrer-ocana <crazyserver@gmail.com>
2015-04-20 16:15:57 +0200

    Add Nodes instalation

--- agora/create_nodes.sh
+++ agora/create_nodes.sh
@@ -14,3 +14,5 @@ mkdir_777 $datadir/wpdata/$usu
 pushd $datadir/wpdata/$usu
 sudo unzip -q /vagrant_git/agora/sql/$template.zip
 popd
+
+sudo -E php /dades/agora/html/wordpress/wp-includes/xtec/scripts/cli.php -s=script_enable_service --ccentre=$usu --password=6142bfd56a583d891f0b1dcdbb2a9ef8 --clientName="$usu VM dev" --clientCode=$usu --clientAddress=Test clientCity=ciutat --clientDNS=$usu --clientPC=000000 --URLNodesModelBase=http://pwc-int.educacio.intranet/agora/master --shortcodes=cfa,pri,sec --DBNodesModel=usu6,usu7,usu8,usu9,usu10
