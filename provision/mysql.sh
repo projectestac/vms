4a67ee985921d78f67783bf95d09173438ab6f93
pau.ferrer-ocana <crazyserver@gmail.com>
2015-04-22 11:26:52 +0200

    Disable phpmysql login

--- provision/mysql.sh
+++ provision/mysql.sh
@@ -18,3 +18,5 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $pass"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
 sudo apt-get install -y phpmyadmin > /dev/null
+
+sudo sed -i "s/.*\['auth_type'\].*/\$cfg['Servers'][\$i]['auth_type'] = 'config';\n\$cfg['Servers'][\$i]['user'] = 'root';\n\$cfg['Servers'][\$i]['password'] = 'agora';/" /etc/phpmyadmin/config.inc.php
