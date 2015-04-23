4f910d3f8d81249b68b231957de58bd7b6492c95
pau.ferrer-ocana <crazyserver@gmail.com>
2015-04-23 10:24:26 +0200

    Add prestatgeria configuration

--- prestatgeria/provision.sh
+++ prestatgeria/provision.sh
@@ -7,7 +7,32 @@ wwwdir=$rootdir/html
 datadir=$rootdir/docs
 git=/git/prestatgeria
 
-
 echo 'Provision Prestatgeria'
 
 mysql_import_db "presta" /vms/prestatgeria/presta.sql
+
+sudo cp $wwwdir/config/env-config-dist.php $wwwdir/config/env-config.php
+sudo cp $wwwdir/config/config-restricted-dist.php $wwwdir/config/config-restricted.php
+sudo cp $wwwdir/htaccess.dist $wwwdir/.htaccess
+
+mkdir_777 $rootdir/centres
+sudo cp -R $git/centres/* $rootdir/centres
+chown_777 $rootdir/centres
+
+mkdir_777 $rootdir/images
+sudo cp -R $git/images/* $rootdir/images
+chown_777 $rootdir/images
+
+mkdir_777 $rootdir/rss
+sudo cp -R $git/rss/* $rootdir/rss
+chown_777 $rootdir/rss
+
+mkdir_777 $rootdir/templates_c
+sudo cp -R $git/templates_c/* $rootdir/templates_c
+chown_777 $rootdir/templates_c
+
+mkdir_777 $rootdir/ztemp
+sudo cp -R $git/ztemp/* $rootdir/ztemp
+chown_777 $rootdir/ztemp
+
+chown 644 $wwwdir/prestatgeria/config/config.php
