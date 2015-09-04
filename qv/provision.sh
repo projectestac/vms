1fdc17c95fb233ce665b93d52aa2d16fda5ce87c
crazyserver <crazyserver@gmail.com>
2015-09-04 13:42:13 +0200

    Add compilation of qv

--- qv/provision.sh
+++ qv/provision.sh
@@ -5,12 +5,22 @@ source "/vms/provision/functions.sh"
 echo 'Provision QV'
 
 rootdir=/dades/qv
+gitdir=/dades/qv
 
 oracle_impdp qv /vms/qv ts_e13_qv
 
 mkdir_777 $rootdir/qv_biblio/work
+cd $gitdir/qv_biblio/
+ant dev
+
 mkdir_777 $rootdir/qv_editor/work
+cd $gitdir/qv_editor/
+ant dev
+
 mkdir_777 $rootdir/qv_viewer/work
-mkdir_777 $rootdir/qv_admin/work
+cd $gitdir/qv_viewer/
+ant dev
+
+#mkdir_777 $rootdir/qv_admin/work
 
 
