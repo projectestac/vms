c7dfcfeea0118802db08d42ade36195930baf372
Pau Ferrer Ocaña <crazyserver@gmail.com>
2015-11-02 11:34:03 +0100

    Add ZER template to nodes

--- agora/create_nodes.sh
+++ agora/create_nodes.sh
@@ -15,4 +15,4 @@ pushd $datadir/wpdata/$usu
 sudo unzip -q /git/agora/sql/$template.zip
 popd
 
-sudo -E php /dades/agora/html/wordpress/wp-includes/xtec/scripts/cli.php -s=script_enable_service --ccentre=$usu --password=6142bfd56a583d891f0b1dcdbb2a9ef8 --clientName="$usu VM dev" --clientCode=$usu --clientAddress=Test clientCity=ciutat --clientDNS=$usu --clientPC=000000 --URLNodesModelBase=http://pwc-int.educacio.intranet/agora/master --shortcodes=cfa,pri,sec --DBNodesModel=usu6,usu7,usu8,usu9,usu10
+sudo -E php /dades/agora/html/wordpress/wp-includes/xtec/scripts/cli.php -s=script_enable_service --ccentre=$usu --password=6142bfd56a583d891f0b1dcdbb2a9ef8 --clientName="$usu VM dev" --clientCode=$usu --clientAddress=Test clientCity=ciutat --clientDNS=$usu --clientPC=000000 --URLNodesModelBase=http://pwc-int.educacio.intranet/agora/master --shortcodes=cfa,pri,sec,zer --DBNodesModel=usu6,usu7,usu8,usu9,usu10
--- agora/provision.sh
+++ agora/provision.sh
@@ -64,7 +64,7 @@ execute_in_oracle "@/dades/agora/html/moodle2/lib/dml/oci_native_moodle_package.
 /vms/agora/create_nodes.sh usu1 $rootdir pri
 /vms/agora/create_nodes.sh usu2 $rootdir sec
 /vms/agora/create_nodes.sh usu3 $rootdir cfa
-/vms/agora/create_nodes.sh usu4 $rootdir pri
+/vms/agora/create_nodes.sh usu4 $rootdir zer
 #/vms/agora/create_nodes.sh usu5 $rootdir eoi
 
 #Finish instaling portal
