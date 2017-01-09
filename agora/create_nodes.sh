8e004391f01abd63509b5981522c65755060f9de
Toni Ginard <toni.ginard@gmail.com>
2017-01-09 19:33:08 +0100

    NODES: Allow nom propi different from database name

--- agora/create_nodes.sh
+++ agora/create_nodes.sh
@@ -16,4 +16,4 @@ pushd $datadir/$usu
 sudo unzip -q /git/agora/sql/$template.zip
 popd
 
-sudo -E php /dades/agora/html/wordpress/wp-includes/xtec/scripts/cli.php -s=script_enable_service --ccentre=$usu --password=6142bfd56a583d891f0b1dcdbb2a9ef8 --clientName="$usu VM dev" --clientCode=$usu --clientAddress=Test clientCity=ciutat --clientDNS=$nompropi --clientPC=000000 --origin_url=http://pwc-int.educacio.intranet/agora/$template --origin_bd=$5
+sudo -E php /dades/agora/html/wordpress/wp-includes/xtec/scripts/cli.php -s=script_enable_service --ccentre=$nompropi --password=6142bfd56a583d891f0b1dcdbb2a9ef8 --clientName="$usu VM dev" --clientCode=$usu --clientAddress=Test clientCity=ciutat --clientDNS=$nompropi --clientPC=000000 --origin_url=http://pwc-int.educacio.intranet/agora/$template --origin_bd=$5
