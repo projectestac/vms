e6839a965d567bba03c566376d894c0e408be72c
Toni Ginard <toni.ginard@gmail.com>
2019-06-21 12:25:10 +0200

    Fixed install of php-memcached

--- provision/php7.sh
+++ provision/php7.sh
@@ -88,8 +88,8 @@ echo "opcache.enable_file_override = 0" | sudo tee -a /etc/php/7.0/mods-availabl
 
 #sudo ln -s /etc/php/7.0/mods-available/opcache.ini /etc/php/7.0/apache2/conf.d/20-opcache.ini
 
-echo 'Install memcache'
-sudo apt-get install -y php7.0-memcache memcached
+echo 'Install memcached'
+sudo apt-get install -y php7.0-memcached memcached
 
 sudo service apache2 restart
 
