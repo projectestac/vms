33a12508a676454505b15226aa2802bd2dba37a1
Toni Ginard <toni.ginard@ticxcat.cat>
2019-07-03 23:01:26 +0200

    Increased maximum execution time from 5 to 15 minutes to avoid interruptions on Moodle upgrades

--- provision/php7.sh
+++ provision/php7.sh
@@ -40,7 +40,7 @@ sudo sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.0/apache2/ph
 sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.0/apache2/php.ini
 sudo sed -i "s/allow_url_fopen = .*/allow_url_fopen = Off/" /etc/php/7.0/apache2/php.ini
 sudo sed -i "s/;error_log = php_errors.log/error_log = \/var\/log\/apache2\/php_errors.log/" /etc/php/7.0/apache2/php.ini
-sudo sed -i "s/max_execution_time = .*/max_execution_time = 300/" /etc/php/7.0/apache2/php.ini
+sudo sed -i "s/max_execution_time = .*/max_execution_time = 900/" /etc/php/7.0/apache2/php.ini
 
 sudo sed -i '$ a\date.timezone = "Europe/Madrid"' /etc/php/7.0/cli/php.ini
 sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/cli/php.ini
@@ -50,7 +50,7 @@ sudo sed -i "s/display_startup_errors = .*/display_startup_errors = On/" /etc/ph
 # Next line is commented on cli to allow composer install
 #sudo sed -i "s/allow_url_fopen = .*/allow_url_fopen = Off/" /etc/php/7.0/cli/php.ini
 sudo sed -i "s/;error_log = php_errors.log/error_log = \/var\/log\/apache2\/php_errors.log/" /etc/php/7.0/cli/php.ini
-sudo sed -i "s/max_execution_time = .*/max_execution_time = 300/" /etc/php/7.0/cli/php.ini
+sudo sed -i "s/max_execution_time = .*/max_execution_time = 900/" /etc/php/7.0/cli/php.ini
 #sudo sed -i "s/disable_functions = .*/disable_functions = /" /etc/php/7.0/cli/php.ini
 
 #Log
