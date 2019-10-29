4a2496ca35054fda28f0031c493c90713dc80311
Toni Ginard <toni.ginard@gmail.com>
2019-10-29 17:32:15 +0100

    Install MySQL in quiet mode

--- provision/mysql.sh
+++ provision/mysql.sh
@@ -8,10 +8,9 @@ echo 'Install MySQL'
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $pass"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $pass"
 
-sudo apt-get install -y mysql-server-5.7
+sudo apt-get install -qq mysql-server-5.7 &> /dev/null
 sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/my.cnf
 sudo sed -i 's/\[mysqld\]/\[mysqld\]\nwait_timeout = 100\nmax_connections=500/g' /etc/mysql/my.cnf
-
 
 sudo service mysql restart
 
@@ -20,7 +19,8 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm pass
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $pass"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $pass"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-sudo apt-get install -y phpmyadmin &> /dev/null
+
+sudo apt-get install -qq phpmyadmin &> /dev/null
 
 sudo sed -i "s/.*\['auth_type'\].*/\$cfg['Servers'][\$i]['auth_type'] = 'config';\n\$cfg['Servers'][\$i]['user'] = 'root';\n\$cfg['Servers'][\$i]['password'] = 'agora';/" /etc/phpmyadmin/config.inc.php
 sudo sed -i '/^.*open_basedir/ s/$/:\/tmp\//' /etc/apache2/conf-available/phpmyadmin.conf
