2779667f341662c60b6d39944ec1c21cf151d11e
Toni Ginard <toni.ginard@gmail.com>
2019-10-29 18:35:08 +0100

    Added support for postgresql

--- provision.sh
+++ provision.sh
@@ -5,6 +5,7 @@ source "/vms/provision/functions.sh"
 /vms/provision/base.sh
 /vms/provision/php7.sh
 /vms/provision/mysql.sh $pass
+/vms/provision/postgresql.sh $pass
 /vms/provision/oracle-extended.sh default
 #/vms/provision/moodlemobile.sh
 
---
+++ provision/postgresql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pass=$1
+
+echo 'Install PostgreSQL'
+
+sudo apt-get install -qq postgresql postgresql-contrib &> /dev/null
+
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.5/main/postgresql.conf
+sudo echo "host   all all 0.0.0.0/0   md5" >> /etc/postgresql/9.5/main/pg_hba.conf
+
+sudo service postgresql restart
+
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD '$pass';"
+
