6cc3b94d56ee02058f40a4215bc3041eff2ee903
Toni Ginard <toni.ginard@gmail.com>
2019-12-12 11:47:56 +0100

    Changed owner of database and tables

--- agora/create_moodle_postgres.sh
+++ agora/create_moodle_postgres.sh
@@ -11,8 +11,8 @@ export PGPASSWORD=$pass
 
 psql -U postgres -h localhost -c "CREATE USER $usu WITH ENCRYPTED PASSWORD '$pass';" &> /dev/null
 psql -U postgres -h localhost -c "ALTER ROLE $usu Superuser;" &> /dev/null
-psql -U postgres -h localhost -c "CREATE DATABASE $usu LC_COLLATE ='ca_ES.UTF-8' LC_CTYPE = 'ca_ES.UTF-8' TEMPLATE template0;" &> /dev/null
-psql -U postgres -h localhost -d $usu -1 -f /vms/agora/usu1-moodle.sql &> /dev/null
+psql -U postgres -h localhost -c "CREATE DATABASE $usu OWNER $usu LC_COLLATE ='ca_ES.UTF-8' LC_CTYPE = 'ca_ES.UTF-8' TEMPLATE template0;" &> /dev/null
+psql -U $usu -h localhost -d $usu -1 -f /vms/agora/usu1-moodle.sql &> /dev/null
 
 mkdir_777 $datadir/moodledata/$usu
 
