fcefb1fba04f0cf3fa9c3bc9dd263521f1071e47
Toni Ginard <toni.ginard@gmail.com>
2020-06-16 14:29:42 +0200

    Changes in php.ini: max_input_vars and max_execution_time

--- provision/php7.sh
+++ provision/php7.sh
@@ -32,9 +32,9 @@ sudo sed -i '$ a\date.timezone = "Europe/Madrid"' /etc/php/7.3/apache2/php.ini
 sudo sed -i "s/memory_limit = .*/memory_limit = 256M/" /etc/php/7.3/apache2/php.ini
 sudo sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.3/apache2/php.ini
 sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/max_input_vars = .*/max_input_vars = 2000/" /etc/php/7.3/apache2/php.ini
+sudo sed -i "s/;max_input_vars = .*/max_input_vars = 4000/" /etc/php/7.3/apache2/php.ini
 sudo sed -i "s/allow_url_fopen = .*/allow_url_fopen = On/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/max_execution_time = .*/max_execution_time = 300/" /etc/php/7.3/apache2/php.ini
+sudo sed -i "s/max_execution_time = .*/max_execution_time = 600/" /etc/php/7.3/apache2/php.ini
 sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/apache2/php.ini
 sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/apache2/php.ini
 sudo sed -i "s/display_startup_errors = .*/display_startup_errors = On/" /etc/php/7.3/apache2/php.ini
