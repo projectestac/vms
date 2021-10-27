08efcbf358306a8c43cf96218d5b4f61455271b2
Toni Ginard <toni.ginard@gmail.com>
2021-10-27 16:29:16 +0200

    Improved export script

--- alexandria/export.sh
+++ alexandria/export.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-rm /git/alexandria/alexandria-38-pg.sql
-pg_dump -U postgres -h localhost alexandria -O -x >> /git/alexandria/alexandria-38-pg.sql
+rm /git/alexandria/dump/alexandria-pg.sql
+pg_dump -U postgres -h localhost alexandria -O -x >> /git/alexandria/dump/alexandria-pg.sql
 
 pushd /dades/data
-tar czfp /git/alexandria/alexandriadata.tar.gz *
+rm /git/alexandria/dump/alexandriadata.tar.gz
+tar czfp /git/alexandria/dump/alexandriadata.tar.gz *
 popd
