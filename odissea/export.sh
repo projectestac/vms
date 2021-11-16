f4c739b40b86ee2c797f0bcf39030dc9d3d379a8
Toni Ginard <toni.ginard@gmail.com>
2021-11-16 13:45:43 +0100

    Fixed export script and changed database dump file name

--- odissea/export.sh
+++ odissea/export.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-rm /git/odissea/odissea-38-pg.sql
-pg_dump -U postgres -h localhost odissea -O -x >> /git/odissea/odissea-38-pg.sql
+rm /git/odissea/dump/odissea-pg.sql
+pg_dump -U postgres -h localhost odissea -O -x >> /git/odissea/dump/odissea-pg.sql
 
 pushd /dades/data
-tar czfp /git/odissea/odisseadata.tar.gz *
+tar czfp /git/odissea/dump/odisseadata.tar.gz *
 popd
