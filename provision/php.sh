9d3b5ce80e972d2677e984e642a01857b0f223f8
Toni Ginard <toni.ginard@gmail.com>
2023-04-25 13:56:29 +0200

    Upgraded to PHP 7.4

--- provision/php.sh
+++ provision/php.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo 'Installing PHP 7.3...'
+echo 'Installing PHP 7.4...'
 
-sudo add-apt-repository ppa:ondrej/php &> /dev/null
+#sudo add-apt-repository ppa:ondrej/php &> /dev/null
 sudo apt-get update &> /dev/null
 
-sudo apt-get install -qq apache2 php7.3 php7.3-curl php7.3-tidy php7.3-gd php7.3-xml php7.3-xmlrpc php7.3-intl php7.3-cli php-pear php7.3-dev libapache2-mod-php7.3 php-codesniffer php7.3-mbstring php7.3-pgsql php7.3-mysql php7.3-gettext php7.3-zip php7.3-soap php-sodium php7.3-tokenizer php7.3-imagick&> /dev/null
+sudo apt-get install -qq apache2 php7.4 php7.4-curl php7.4-tidy php7.4-gd php7.4-xml php7.4-xmlrpc php7.4-intl php7.4-cli php-pear php7.4-dev php7.4-ldap libapache2-mod-php7.4 php-codesniffer php7.4-mbstring php7.4-pgsql php7.4-mysql php7.4-gettext php7.4-zip php7.4-soap php7.4-tokenizer &> /dev/null
 
 sudo mkdir /etc/apache2/sites-agora
 sudo cp -R /vms/provision/conf/* /etc/apache2/sites-agora
@@ -27,26 +27,26 @@ sudo a2ensite default-ssl
 sudo service apache2 start
 
 #PHP Configuration
-sudo sed -i '$ a\date.timezone = "Europe/Madrid"' /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/memory_limit = .*/memory_limit = 256M/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/;max_input_vars = .*/max_input_vars = 5000/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/allow_url_fopen = .*/allow_url_fopen = On/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/max_execution_time = .*/max_execution_time = 600/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/display_startup_errors = .*/display_startup_errors = On/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/;error_log = php_errors.log/error_log = \/var\/log\/apache2\/php_errors.log/" /etc/php/7.3/apache2/php.ini
+sudo sed -i '$ a\date.timezone = "Europe/Madrid"' /etc/php/7.4/apache2/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 256M/" /etc/php/7.4/apache2/php.ini
+sudo sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.4/apache2/php.ini
+sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.4/apache2/php.ini
+sudo sed -i "s/;max_input_vars = .*/max_input_vars = 4000/" /etc/php/7.4/apache2/php.ini
+sudo sed -i "s/allow_url_fopen = .*/allow_url_fopen = On/" /etc/php/7.4/apache2/php.ini
+sudo sed -i "s/max_execution_time = .*/max_execution_time = 600/" /etc/php/7.4/apache2/php.ini
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.4/apache2/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.4/apache2/php.ini
+sudo sed -i "s/display_startup_errors = .*/display_startup_errors = On/" /etc/php/7.4/apache2/php.ini
+sudo sed -i "s/;error_log = php_errors.log/error_log = \/var\/log\/apache2\/php_errors.log/" /etc/php/7.4/apache2/php.ini
 
-sudo sed -i '$ a\date.timezone = "Europe/Madrid"' /etc/php/7.3/cli/php.ini
-sudo sed -i "s/memory_limit = .*/memory_limit = 256M/" /etc/php/7.3/cli/php.ini
-sudo sed -i "s/max_input_vars = .*/max_input_vars = 5000/" /etc/php/7.3/cli/php.ini
-sudo sed -i "s/allow_url_fopen = .*/allow_url_fopen = On/" /etc/php/7.3/cli/php.ini
-sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/cli/php.ini
-sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/cli/php.ini
-sudo sed -i "s/display_startup_errors = .*/display_startup_errors = On/" /etc/php/7.3/cli/php.ini
-sudo sed -i "s/;error_log = php_errors.log/error_log = \/var\/log\/apache2\/php_errors.log/" /etc/php/7.3/cli/php.ini
+sudo sed -i '$ a\date.timezone = "Europe/Madrid"' /etc/php/7.4/cli/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 256M/" /etc/php/7.4/cli/php.ini
+sudo sed -i "s/max_input_vars = .*/max_input_vars = 2000/" /etc/php/7.4/cli/php.ini
+sudo sed -i "s/allow_url_fopen = .*/allow_url_fopen = On/" /etc/php/7.4/cli/php.ini
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.4/cli/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.4/cli/php.ini
+sudo sed -i "s/display_startup_errors = .*/display_startup_errors = On/" /etc/php/7.4/cli/php.ini
+sudo sed -i "s/;error_log = php_errors.log/error_log = \/var\/log\/apache2\/php_errors.log/" /etc/php/7.4/cli/php.ini
 
 # Log
 sudo sed -i "s/create 640.*/create 777 vagrant vagrant/" /etc/logrotate.d/apache2
@@ -59,30 +59,30 @@ sudo chown -R vagrant /var/lock/apache2
 sudo adduser vagrant www-data
 
 # Configure opcache
-echo "opcache.enable=1" | sudo tee -a /etc/php/7.3/mods-available/opcache.ini
-echo "opcache.memory_consumption=256" | sudo tee -a /etc/php/7.3/mods-available/opcache.ini
-echo "opcache.max_accelerated_files=4000" | sudo tee -a /etc/php/7.3/mods-available/opcache.ini
+echo "opcache.enable=1" | sudo tee -a /etc/php/7.4/mods-available/opcache.ini
+echo "opcache.memory_consumption=256" | sudo tee -a /etc/php/7.4/mods-available/opcache.ini
+echo "opcache.max_accelerated_files=4000" | sudo tee -a /etc/php/7.4/mods-available/opcache.ini
 # 0 for development, 60 for production
-echo "opcache.revalidate_freq=0" | sudo tee -a /etc/php/7.3/mods-available/opcache.ini
-echo "opcache.interned_strings_buffer=8" | sudo tee -a /etc/php/7.3/mods-available/opcache.ini
-echo "opcache.enable_cli=1" | sudo tee -a /etc/php/7.3/mods-available/opcache.ini
-echo "; Required for Moodle" | sudo tee -a /etc/php/7.3/mods-available/opcache.ini
-echo "opcache.use_cwd = 1" | sudo tee -a /etc/php/7.3/mods-available/opcache.ini
-echo "opcache.validate_timestamps = 1" | sudo tee -a /etc/php/7.3/mods-available/opcache.ini
-echo "opcache.save_comments = 1" | sudo tee -a /etc/php/7.3/mods-available/opcache.ini
-echo "opcache.enable_file_override = 0" | sudo tee -a /etc/php/7.3/mods-available/opcache.ini
+echo "opcache.revalidate_freq=0" | sudo tee -a /etc/php/7.4/mods-available/opcache.ini
+echo "opcache.interned_strings_buffer=8" | sudo tee -a /etc/php/7.4/mods-available/opcache.ini
+echo "opcache.enable_cli=1" | sudo tee -a /etc/php/7.4/mods-available/opcache.ini
+echo "; Required for Moodle" | sudo tee -a /etc/php/7.4/mods-available/opcache.ini
+echo "opcache.use_cwd = 1" | sudo tee -a /etc/php/7.4/mods-available/opcache.ini
+echo "opcache.validate_timestamps = 1" | sudo tee -a /etc/php/7.4/mods-available/opcache.ini
+echo "opcache.save_comments = 1" | sudo tee -a /etc/php/7.4/mods-available/opcache.ini
+echo "opcache.enable_file_override = 0" | sudo tee -a /etc/php/7.4/mods-available/opcache.ini
 
 echo 'Installing memcached and redis...'
-sudo apt-get install -qq php7.3-memcached php7.3-redis memcached redis-server &> /dev/null
+sudo apt-get install -qq php7.4-memcached php-redis memcached redis-server &> /dev/null
 
 #echo 'Installing XDebug...'
-#sudo apt-get install -qq php7.3-xdebug &> /dev/null
+#sudo apt-get install -qq php7.4-xdebug &> /dev/null
 
-#echo "xdebug.mode=debug" | sudo tee -a /etc/php/7.3/apache2/conf.d/20-xdebug.ini
-#echo "xdebug.idekey=\"vagrant\"" | sudo tee -a /etc/php/7.3/apache2/conf.d/20-xdebug.ini
-#echo "xdebug.cli_color=0" | sudo tee -a /etc/php/7.3/apache2/conf.d/20-xdebug.ini
-#echo "xdebug.start_with_request=no" | sudo tee -a /etc/php/7.3/apache2/conf.d/20-xdebug.ini
-#echo "xdebug.client_port=9003" | sudo tee -a /etc/php/7.3/apache2/conf.d/20-xdebug.ini
-#echo "xdebug.client_host=10.0.2.15 " | sudo tee -a /etc/php/7.3/apache2/conf.d/20-xdebug.ini
+#echo "xdebug.mode=debug" | sudo tee -a /etc/php/7.4/apache2/conf.d/20-xdebug.ini
+#echo "xdebug.idekey=\"vagrant\"" | sudo tee -a /etc/php/7.4/apache2/conf.d/20-xdebug.ini
+#echo "xdebug.cli_color=0" | sudo tee -a /etc/php/7.4/apache2/conf.d/20-xdebug.ini
+#echo "xdebug.start_with_request=no" | sudo tee -a /etc/php/7.4/apache2/conf.d/20-xdebug.ini
+#echo "xdebug.client_port=9003" | sudo tee -a /etc/php/7.4/apache2/conf.d/20-xdebug.ini
+#echo "xdebug.client_host=10.0.2.15 " | sudo tee -a /etc/php/7.4/apache2/conf.d/20-xdebug.ini
 
 sudo service apache2 restart
