5b1f5d5b1d7db0e49cfcadf1c763bc0cdba86bc1
Toni Ginard <toni.ginard@gmail.com>
2024-06-10 18:38:23 +0200

    Added php8.1-imagick

--- provision/php.sh
+++ provision/php.sh
@@ -5,7 +5,7 @@ echo 'Installing PHP 8.1...'
 #sudo add-apt-repository ppa:ondrej/php &> /dev/null
 sudo apt-get update &> /dev/null
 
-sudo apt-get install -qq apache2 php8.1 php8.1-curl php8.1-tidy php8.1-gd php8.1-xml php8.1-xmlrpc php8.1-intl php8.1-cli php-pear php8.1-dev php8.1-ldap libapache2-mod-php8.1 php-codesniffer php8.1-mbstring php8.1-pgsql php8.1-mysql php8.1-gettext php8.1-zip php8.1-soap php8.1-tokenizer &> /dev/null
+sudo apt-get install -qq apache2 php8.1 php8.1-curl php8.1-tidy php8.1-gd php8.1-imagick php8.1-xml php8.1-xmlrpc php8.1-intl php8.1-cli php-pear php8.1-dev php8.1-ldap libapache2-mod-php8.1 php-codesniffer php8.1-mbstring php8.1-pgsql php8.1-mysql php8.1-gettext php8.1-zip php8.1-soap php8.1-tokenizer &> /dev/null
 
 sudo mkdir /etc/apache2/sites-agora
 sudo cp -R /vms/provision/conf/agora.conf /etc/apache2/sites-agora/agora.conf
